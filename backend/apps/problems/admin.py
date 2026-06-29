ba8a4c46e0903fa76f178544377b98b3fa07b588
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2026-06-29 14:12:11 +0530

    Added problems import functionality

--- backend/apps/problems/admin.py
+++ backend/apps/problems/admin.py
@@ -1,9 +1,45 @@
 from django.contrib import admin
-from django.db import models
-from martor.widgets import AdminMartorWidget
 from .models import Problem
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import Widget
+
+class NativeJSONDictWidget(Widget):
+    def clean(self, value, row=None, **kwargs):
+        if value is None:
+            return {}
+
+        if isinstance(value, dict):
+            return value
+
+        raise ValueError(f"Expected dict, got {type(value).__name__}")
+
+
+class NativeJSONListWidget(Widget):
+    def clean(self, value, row=None, **kwargs):
+        if value is None:
+            return []
+
+        if isinstance(value, list):
+            return value
+
+        raise ValueError(f"Expected list, got {type(value).__name__}")
+
+class ProblemResources(resources.ModelResource):
+    starter_code = fields.Field(column_name="starter_code", attribute="starter_code", widget=NativeJSONDictWidget())
+    test_cases = fields.Field(column_name="test_cases", attribute="test_cases", widget=NativeJSONListWidget())
+    input_types = fields.Field(column_name="input_types", attribute="input_types", widget=NativeJSONDictWidget())
+    output_type = fields.Field(column_name="output_type", attribute="output_type", widget=NativeJSONDictWidget())
+
+    class Meta:
+        import_id_fields = ("title",)
+        model = Problem
+        fields = ("title", "difficulty", "category", "description", "starter_code", "test_cases", "order_matters", "input_types", "output_type" )
+
+        skip_unchanged = True
+        report_skipped = True
 
 # Register your models here.
 @admin.register(Problem)
-class ProblemAdmin(admin.ModelAdmin):
-    pass
+class ProblemAdmin(ImportExportModelAdmin):
+    resource_classes = [ProblemResources]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "martor",
     "django_cookiejwt",
     "drf_spectacular",
+    "import_export"
 ]
 
 MIDDLEWARE = [
