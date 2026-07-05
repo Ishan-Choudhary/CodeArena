0e5e5464039d17771ef29754e9288a21fb39916b
Ishan-Choudhary <ishan.dchoudhary@gmail.com>
2026-07-05 12:40:14 +0530

    Updated production setting to use https for jwt and header config to included forwarded protocol header

--- backend/config/settings_prod.py
+++ backend/config/settings_prod.py
@@ -7,8 +7,14 @@ ALLOWED_HOSTS = allowed_hosts_env.split(",") if allowed_hosts_env else []
 
 frontend_url = os.environ.get("FRONTEND_URL", "")
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [frontend_url] if frontend_url else []
-CSRF_TRUSTED_ORIGINS = [frontend_url] if frontend_url else []
+CORS_ALLOWED_ORIGINS = frontend_url.split(",") if frontend_url else []
+CSRF_TRUSTED_ORIGINS = frontend_url.split(",") if frontend_url else []
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+COOKIEJWT_HTTPONLY = True
+COOKIEJWT_SECURE = True
+COOKIEJWT_SAMESITE = 'Lax'
